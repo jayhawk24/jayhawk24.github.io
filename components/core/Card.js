a35b4b6bd47090f52aba296f9c38d66d6f92684b
anubhav-gig <dev.anubhav@gigwalla.com>
2021-11-04 14:03:28 +0530

    Add project v80

--- components/core/Card.js
+++ components/core/Card.js
@@ -1,28 +1,35 @@
 import React from "react";
 
 export default function Card({ title, imgSrc, desc, link }) {
-  return (
-    <div
-      className="card text-white bg-dark"
-      style={{ boxShadow: "10px -10px #397ca4" }}
-    >
-      <img
-        className="card-img-top"
-        src={imgSrc}
-        alt="Card Project head cap"
-        height="250px"
-        width="400px"
-        style={{
-          userSelect: "none"
-        }}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{desc}</p>
-        <a href={link} className="btn btn-primary">
-          View Project
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className="card text-white bg-dark"
+            style={{ boxShadow: "10px -10px #397ca4" }}
+        >
+            <div
+                style={{
+                    height: "250px",
+                    width: "450px",
+                    overflow: "hidden",
+                    margin: "0 auto"
+                }}
+            >
+                <img
+                    className="card-img-top img-responsive"
+                    src={imgSrc}
+                    alt="Card Project head cap"
+                    style={{
+                        userSelect: "none"
+                    }}
+                />
+            </div>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{desc}</p>
+                <a href={link} className="btn btn-primary">
+                    View Project
+                </a>
+            </div>
+        </div>
+    );
 }
