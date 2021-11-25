670ca2c3ca03e55853e0e20b52c83c009e55c5be
Anubhav Gupta <anubhavthrilling24@gmail.com>
2021-11-25 21:11:13 +0530

    Fix project cards background

--- components/core/Card.js
+++ components/core/Card.js
@@ -1,27 +1,28 @@
-import React from "react";
+import React from 'react';
 
 export default function Card({ title, imgSrc, desc, link }) {
     return (
         <div
             className="card text-white bg-dark"
-            style={{ boxShadow: "10px -10px #397ca4" }}
+            style={{ boxShadow: '10px -10px #397ca4', overflow: 'hidden' }}
         >
             <div
                 style={{
-                    height: "250px",
-                    width: "450px",
-                    overflow: "hidden",
-                    margin: "0 auto"
+                    height: '250px',
+                    width: '450px',
+                    overflow: 'hidden',
+                    margin: '0 auto'
                 }}
             >
-                <img
+                <div
                     className="card-img-top img-responsive"
-                    src={imgSrc}
                     alt="Card Project head cap"
                     style={{
-                        userSelect: "none"
+                        userSelect: 'none',
+                        height: '100%',
+                        background: `url(${imgSrc}) no-repeat scroll center top / cover`
                     }}
-                />
+                ></div>
             </div>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
