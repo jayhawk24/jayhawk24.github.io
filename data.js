bcc8ce207313595cda4c524c8905f0058748c46d
anubhav-sl <anubhav@steinnlabs.com>
2022-05-29 20:12:59 +0530

    Fix subtitle

--- data.js
+++ data.js
@@ -1,83 +1,83 @@
 export const home = {
-    domain: 'jayhawk.github.io',
-    name: 'Anubhav',
-    subtitle: 'A Freelance Web Developer from India.',
-    email: 'anubhavthrilling24@gmail.com',
+    domain: "jayhawk.github.io",
+    name: "Anubhav",
+    subtitle: "A Freelance Full-stack Engineer from India.",
+    email: "anubhavthrilling24@gmail.com",
     resumeLink:
-        'https://drive.google.com/file/d/1sqxhKQnX9vD1Q41R1QqjCUadxBZlfTJ-/view?usp=sharing',
-    desc: 'A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks.'
+        "https://drive.google.com/file/d/1sqxhKQnX9vD1Q41R1QqjCUadxBZlfTJ-/view?usp=sharing",
+    desc: "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks."
 };
 
 export const socials = {
     // Only usernames.
-    twitter: 'jayh4wk',
-    github: 'jayhawk24',
-    instagram: '__jayhawk__'
+    twitter: "jayh4wk",
+    github: "jayhawk24",
+    instagram: "__jayhawk__"
 };
 
 export const about = {
     aboutMe:
-        '🙏 Namaste, I am Anubhav Gupta from Varanasi, India I am a Full Stack Developer at Gigwalla. Apart from coding, there are a lot of activities I love to do like...',
-    hobbies: ['Basketball', 'Chess', 'Hiking', 'Swimming'],
-    quote: 'Write code that changes Lives...',
+        "🙏 Namaste, I am Anubhav Gupta from Varanasi, India I am a Full Stack Developer at Gigwalla. Apart from coding, there are a lot of activities I love to do like...",
+    hobbies: ["Basketball", "Chess", "Hiking", "Swimming"],
+    quote: "Write code that changes Lives...",
     skills: [
-        'nextjs',
-        'react',
-        'redux',
-        'python',
-        'bash',
-        'cplusplus',
-        'css3',
-        'sass',
-        'typescript',
-        'docker',
-        'django',
-        'html5',
-        'git',
-        'javascript',
-        'jquery',
-        'mongodb',
-        'nodejs',
-        'php'
+        "nextjs",
+        "react",
+        "redux",
+        "python",
+        "bash",
+        "cplusplus",
+        "css3",
+        "sass",
+        "typescript",
+        "docker",
+        "django",
+        "html5",
+        "git",
+        "javascript",
+        "jquery",
+        "mongodb",
+        "nodejs",
+        "php"
     ],
-    tools: ['github', 'photoshop', 'vim', 'visualstudio', 'linux']
+    tools: ["github", "photoshop", "vim", "visualstudio", "linux"]
 };
 
 export const projects = [
     {
-        title: 'V80 Stocks',
-        imgSrc: 'assets/v80.jpg',
+        title: "V80 Stocks",
+        imgSrc: "assets/v80.jpg",
         desc: "List of stocks below 200 DMA which are in Vivek Singhal's v40 and v40 next stocks list.",
-        link: 'https://github.com/jayhawk24/v80'
+        link: "https://github.com/jayhawk24/v80"
     },
     {
-        title: 'E Store 24',
-        imgSrc: 'assets/estore.png',
-        desc: 'The website resembles a real store and you can add products to your cart and pay for them.',
-        link: 'https://github.com/jayhawk24/E-store'
+        title: "E Store 24",
+        imgSrc: "assets/estore.png",
+        desc: "The website resembles a real store and you can add products to your cart and pay for them.",
+        link: "https://github.com/jayhawk24/E-store"
     },
     {
-        title: 'Blood Bank',
-        imgSrc: 'assets/donation.jpg',
-        desc: 'Web application for hospitals to manage their blood bank and users who need blood. Made with PHP and MySQL.',
-        link: 'https://github.com/jayhawk24/blood_bank'
+        title: "Blood Bank",
+        imgSrc: "assets/donation.jpg",
+        desc: "Web application for hospitals to manage their blood bank and users who need blood. Made with PHP and MySQL.",
+        link: "https://github.com/jayhawk24/blood_bank"
     },
     {
-        title: 'Ora Studio',
-        imgSrc: 'assets/orastudio.png',
-        desc: 'Architect Organisation Website. It is astatic website but with a great userinterface and you can check it out.',
-        link: 'https://github.com/jayhawk24/orastudio'
+        title: "Ora Studio",
+        imgSrc: "assets/orastudio.png",
+        desc: "Architect Organisation Website. It is astatic website but with a great userinterface and you can check it out.",
+        link: "https://github.com/jayhawk24/orastudio"
     },
     {
-        title: 'Zen Social',
-        imgSrc: 'assets/zensocial.png',
-        desc: 'Social App made with django. You can post your thoughts and ideas anonymously and also create groups and post there.',
-        link: 'https://github.com/jayhawk24/zensocial'
+        title: "Zen Social",
+        imgSrc: "assets/zensocial.png",
+        desc: "Social App made with django. You can post your thoughts and ideas anonymously and also create groups and post there.",
+        link: "https://github.com/jayhawk24/zensocial"
     },
     {
-        title: 'Take Turns',
-        imgSrc: 'assets/takeTurns.png',
+        title: "Take Turns",
+        imgSrc: "assets/takeTurns.png",
         desc: "Easy Friendy Management, register your roommates, and it will simply show who's turn it is to carry out a task job.",
-        link: 'https://github.com/jayhawk24/take-turns'
+        link: "https://github.com/jayhawk24/take-turns"
     }
 ];
